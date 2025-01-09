8c0438032f4596bd416b4ea534987f78767a614e
SaidBC <razzouksaid139@gmail.com>
2025-01-09 01:33:18 +0100

    revert : change hard coded url to SERVER_URL

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-const socket = io("http://localhost:8000/", { autoConnect: false });
+const socket = io(SERVER_URL, { autoConnect: false });
 
 export default socket;
